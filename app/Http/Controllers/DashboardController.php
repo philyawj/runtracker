8123b85195e11712a1585a34d81f0b2d7f194667
Jordan Philyaw <jordanphilyaw@gmail.com>
2019-07-11 11:04:16 -0700

    ability to find first date and last date

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -28,24 +28,38 @@ class DashboardController extends Controller
     public function index()
     {
 
+        $user_id = Auth::user()->id;
+
+        $alldates = Run::all()->where('user_id', $user_id);
+        $firstdate = $alldates->sortBy('date')->first()->date;
+        $lastdate = $alldates->sortByDesc('date')->first()->date;
+        echo 'The first date is ' . $firstdate . '  and the last date is ' . $lastdate;
+
         // only return last 3 runs from logged in user
-        $lastthreeruns = Run::all()->where('user_id', Auth::user()->id)->sortByDesc('date')->take(3);
+        // $lastthreeruns = Run::all()->where('user_id', $user_id)->sortByDesc('date')->take(3);
+        $lastthreeruns = Run::all();
 
         // how many miles run thus far this week
         $currentweek = Carbon::now()->weekOfYear;
-        $milesthisweek = Run::all()->where('user_id', Auth::user()->id)->where('weekofyear', $currentweek)->sum('miles');
+        // echo $currentweek . " is the current week";
+        // $milesthisweek = Run::all()->where('user_id', $user_id)->where('weekofyear', $currentweek)->sum('miles');
+        // $milesthisweek = Run::where('user_id', $user_id)->where('weekofyear', $currentweek)->get()->sum('miles');
+        $milesthisweek = Run::where('user_id', $user_id)->where('weekofyear', $currentweek)->get()->sum('miles');
+
+        
+        
 
         // how many miles run last month
         $lastweek = Carbon::now()->weekOfYear - 1;
-        $mileslastweek = Run::all()->where('user_id', Auth::user()->id)->where('weekofyear', $lastweek)->sum('miles');
+        $mileslastweek = Run::all()->where('user_id', $user_id)->where('weekofyear', $lastweek)->sum('miles');
 
         // how many miles run thus far this month
         $currentmonth = Carbon::now()->month;
-        $milesthismonth = Run::all()->where('user_id', Auth::user()->id)->where('month', $currentmonth)->sum('miles');
+        $milesthismonth = Run::all()->where('user_id', $user_id)->where('month', $currentmonth)->sum('miles');
 
         // how many miles run last month
         $lastmonth = Carbon::now()->month - 1;
-        $mileslastmonth = Run::all()->where('user_id', Auth::user()->id)->where('month', $lastmonth)->sum('miles');
+        $mileslastmonth = Run::all()->where('user_id', $user_id)->where('month', $lastmonth)->sum('miles');
 
         return view('dashboard', compact('lastthreeruns'), ['milesthisweek' => $milesthisweek, 'mileslastweek' => $mileslastweek, 'milesthismonth' => $milesthismonth, 'mileslastmonth' => $mileslastmonth]);
     }
