070d6fc62eff31ceb87184363820fea6f7f0d996
Jordan Philyaw <jordanphilyaw@gmail.com>
2019-08-18 14:17:53 -0700

    dashboard conditional weekly goal

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,53 +10,61 @@
 
                     <h2>Dashboard</h2>
 
+                    <h3>Miles</h3>
+
                     <div class="row text-center mb-3">
 
-                    <div class="col-6 col-md-3 mb-3 mb-md-0">
-                        <div class="card card-darker h-100">
-                            <div class="card-body">
-                                <h5>Week:</h5>
-                                <h4>{{$milesthisweek}}</h4>
+                        <div class="col-6 col-md-3 mb-3 mb-md-0">
+                            <div class="card card-darker h-100">
+                                <div class="card-body">
+                                    <h5>Week:</h5>
+                                    <h4>{{$milesthisweek}}</h4>
+                                </div>
                             </div>
                         </div>
-                    </div>
 
-                    <div class="col-6 col-md-3 mb-3 mb-md-0">
-                        <div class="card card-darker h-100">
-                            <div class="card-body">
-                                <h5>Month:</h5>
-                                <h4>{{$milesthismonth}}</h4>
+                        <div class="col-6 col-md-3 mb-3 mb-md-0">
+                            <div class="card card-darker h-100">
+                                <div class="card-body">
+                                    <h5>Month:</h5>
+                                    <h4>{{$milesthismonth}}</h4>
+                                </div>
                             </div>
                         </div>
-                    </div>
 
-                    <div class="col-6 col-md-3">
-                        <div class="card card-darker h-100">
-                            <div class="card-body">
-                                <h5>Year:</h5>
-                                <h4>{{$miles_this_year}}</h4>
+                        <div class="col-6 col-md-3">
+                            <div class="card card-darker h-100">
+                                <div class="card-body">
+                                    <h5>Year:</h5>
+                                    <h4>{{$miles_this_year}}</h4>
+                                </div>
                             </div>
                         </div>
-                    </div>
 
-                    <div class="col-6 col-md-3">
-                        <div class="card card-darker h-100">
-                            <div class="card-body">
-                                <h5>All Time:</h5>
-                                <h4>{{$miles_all_time}}</h4>
+                        <div class="col-6 col-md-3">
+                            <div class="card card-darker h-100">
+                                <div class="card-body">
+                                    <h5>All Time:</h5>
+                                    <h4>{{$miles_all_time}}</h4>
+                                </div>
                             </div>
                         </div>
-                    </div>
 
                     </div>
+
+                    <h3>
+                        @if(!is_null($thisweekgoal))
+                            Weekly Goal: {{$thisweekgoal}} 
+                        @else 
+                            Weekly Goal
+                        @endif
+                    </h3>
 
                     @if(is_null($thisweekgoal)) 
-                        <p><a class="btn btn-sm btn-primary" href="{{route('goals.create', [$current_year,$current_week])}}">Button to set goal for this week</a></p>
+                        <p><a class="btn btn-sm btn-primary" href="{{route('goals.create', [$current_year,$current_week])}}">Set goal</a></p>
                         
                     @else
-                        <p>Goal this week: {{$thisweekgoal}}</p>
-                        <p class="mb-0">Progress to weekly goal</p>
-                        <div class="progress">
+                        <div class="progress mb-3">
                             <div class="progress-bar" role="progressbar" style="width: {{$weeklyprogress}}%" aria-valuenow="{{$weeklyprogress}}" aria-valuemin="0" aria-valuemax="100"></div>
                         </div>
                     @endif
@@ -64,7 +72,7 @@
                     
                     
 
-                    <h5>Last 3 Runs</h5>
+                    <h3>Last 6 Weeks</h3>
                     <table class="table">
                             <thead>
                                 <tr>
