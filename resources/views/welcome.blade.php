6f3d36e0a30dcbfd60bf8dae0eee9e77de9d4dfa
Jordan Philyaw <jordanphilyaw@gmail.com>
2019-08-24 11:28:48 -0700

    datepicker colors

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center mt-5">
-        <div class="col-7 col-md-6 col-lg-5 col-xl-4">
+        <div class="col-12 col-sm-8 col-md-6 col-lg-5 col-xl-4">
             <div class="card">
         
                 <div class="card-body text-center">
